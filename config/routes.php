c59172f4c3bd35da3e8e675bd830f0f42e7e5da4
Blake Walters <blakews@bwaltersx.boulder.cpbgroup.com>
2009-09-28 12:10:21 -0600

    changed references to "Nude" to  "Newd"

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,18 @@
 <?php
-// This file is part of Nude.
+// This file is part of Newd.
 // 
-// Nude is free software: you can redistribute it and/or modify
+// Newd is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// Nude is distributed in the hope that it will be useful,
+// Newd is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with Nude.  If not, see <http://www.gnu.org/licenses/>.
+// along with Newd.  If not, see <http://www.gnu.org/licenses/>.
 
 Router::parseExtensions();
 Router::connect('/pages/:action',array('controller' => 'front','action' => ':action'));
--- webroot/js/cpb/modules/media.js
+++ webroot/js/cpb/modules/media.js
@@ -1,7 +1,7 @@
 // 
-// This file is part of Nude.
+// This file is part of Newd.
 // 
-// Nude is free software: you can redistribute it and/or modify
+// Newd is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with Nude.  If not, see <http://www.gnu.org/licenses/>.
+// along with Newd.  If not, see <http://www.gnu.org/licenses/>.
 // 
 // Copyright 2009, Crispin Porter + Bogusky
 
